06f519ca66cbd5ad520c2fe5b5d70278dd8b7a23
Mukund Solanki <mukundsolanki665@gmail.com>
2023-11-28 18:19:48 +0530

    Space Station icon added

--- sample2.py
+++ sample2.py
@@ -1,6 +1,7 @@
 import requests
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 import time
 
 def get_iss_location():
@@ -9,17 +10,23 @@ def get_iss_location():
     iss_position = data['iss_position']
     return float(iss_position['latitude']), float(iss_position['longitude'])
 
-def plot_iss_on_map(m, latitude, longitude):
+def plot_iss_on_map(m, latitude, longitude, icon_path):
     x, y = m(longitude, latitude)
-    m.plot(x, y, 'ro', markersize=12, label='ISS Position')
-    plt.legend()
 
-def update_iss_position(m):
+    # Load the ISS icon image
+    iss_icon = plt.imread(icon_path)
+    imagebox = OffsetImage(iss_icon, zoom=0.1)  # Adjust the zoom factor as needed
+
+    # Create an annotation box at the ISS position
+    ab = AnnotationBbox(imagebox, (x, y), frameon=False, pad=0)
+    m._check_ax().add_artist(ab)
+
+def update_iss_position(m, icon_path):
     while True:
         latitude, longitude = get_iss_location()
         plt.clf()
         m.bluemarble()
-        plot_iss_on_map(m, latitude, longitude)
+        plot_iss_on_map(m, latitude, longitude, icon_path)
         plt.pause(5)  # Pause for 5 seconds before updating
 
 def main():
@@ -28,9 +35,13 @@ def main():
     m.bluemarble()
 
     latitude, longitude = get_iss_location()
-    plot_iss_on_map(m, latitude, longitude)
 
-    update_iss_position(m)
+    # Provide the path to the ISS icon image (replace 'iss_icon.png' with your actual file name)
+    icon_path = 'space-station.png'
+    
+    plot_iss_on_map(m, latitude, longitude, icon_path)
+
+    update_iss_position(m, icon_path)
 
 if __name__ == "__main__":
     main()
